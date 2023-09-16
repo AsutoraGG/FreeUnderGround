dde890943cf6a58f2620f1497e81e813fa73c7bd
AsutoraGG <76235964+AsutoraGG@users.noreply.github.com>
2023-09-16 17:16:39 +0900

    Big Update
    - 右クリック時にメニューを作成
    - 必要ないコード、ボタンを削除
    - 配置、色を変更
    - 大体のコードをbackgroundで処理するように変更

---
+++ background.js
@@ -0,0 +1,51 @@
+const otherFileType = ["wav", "avi", "wmv", "mov"] // working now maybe
+
+function converter(url) { // Main Code
+    for (let i = 0; i < otherFileType.length; i++) { // Check VideoType
+      if (url.includes(otherFileType[i])) {
+        url = url.replace(otherFileType[i], "mp4")
+      }
+    }
+    const parts = url.split('/');
+  
+    if (!isNaN(parts[5])) { // "5/2/6/1"
+      parts[2] = "v.theync.com";
+      parts.splice(3, 2, "videos");
+      parts.pop()
+  
+      return parts.join('/')
+    } else if (parts[5] === "v") { // "v/i/d/e/"
+      parts[4] = "videos"
+      parts.pop()
+  
+      return parts.join('/')
+    }
+  }
+
+chrome.runtime.onInstalled.addListener(function () {
+    const parent = chrome.contextMenus.create({ //右クリック時のメニューを作成
+        id: "share",
+        title: "画像を動画に変換",
+        contexts: ["all"],
+    });
+
+    chrome.contextMenus.onClicked.addListener((info, tab) => { //メニューをクリックしたときの処理
+        if(info.srcUrl && tab.url.startsWith("https://theync")) { //　画像urlがあるか、画像か、theYNCか
+            const convert = converter(info.srcUrl)
+            chrome.tabs.create({
+                url: convert
+            })
+        }
+    })
+
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) { // client(popup.js)からbackgroundに送信されたとき
+        switch(message.action) {
+            case "convertAndOpen": //画像urlが送られてくるのでそれをbackgroundで変換→新しいタブで開く
+                const convertedURL = converter(message.data);
+                chrome.tabs.create({
+                    url: convertedURL
+                });
+            break;
+        }
+    });
+})
--- popup.js
+++ popup.js
@@ -1,38 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const notSupportVideoType = ["wav", "avi", "wmv", "mov"] // working now maybe
-
-  function converter(url) { // Main Code
-    for (let i = 0; i < notSupportVideoType.length; i++) { // Check VideoType
-      if (url.includes(notSupportVideoType[i])) {
-        url = url.replace(notSupportVideoType[i], "mp4")
-      }
-    }
-    const parts = url.split('/');
-
-    if (!isNaN(parts[5])) { // "5/2/6/1"
-      parts[2] = "v.theync.com";
-      parts.splice(3, 2, "videos");
-      parts.pop()
-
-      return parts.join('/')
-    } else if (parts[5] === "v") { // "v/i/d/e/"
-      parts[4] = "videos"
-      parts.pop()
-
-      return parts.join('/')
-    } else { // Unknown URL
-      return alert('Sorry! This is Unknown URL!')
-    }
-  }
-
   document.getElementById('convertButton').addEventListener('click', function () { // Convert
-    const url = document.getElementById('urlInput').value;
-    console.log('clicked')
-    const convertedURL = converter(url);
-    if (convertedURL) {
-      chrome.tabs.create({
-        url: convertedURL
-      });
+    const url = document.getElementById('urlInput').value;　//入力ボックスに入ってるテキストを取得
+    if(url.includes('theync')) { //サムネイル画像がtheYNCか確認
+      chrome.runtime.sendMessage({ action: "convertAndOpen", data: url }); //backgroundに送信
+    }　else {
+      alert('TheYNCの画像ではない可能性があります!')
+      document.getElementById('urlInput').value = '';
     }
   });
 
@@ -42,14 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   })
 
-  document.getElementById('copyvideo').addEventListener('click', function () { // Convert and Copy to Clipboard
-    if (document.getElementById('urlInput').value === "") {
-      return alert('Please put the video thumbnail url!')
-    }
-    navigator.clipboard.writeText(converter(document.getElementById('urlInput').value))
-  })
-
   document.getElementById('cls').addEventListener('click', function () { // CLear Input value
-    document.getElementById('urlInput').value = ''
+    document.getElementById('urlInput').value = '';
   })
 })
